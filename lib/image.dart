6fa5de94ca7da06b54c4ef1ea0173351c4a82864
Wilsonchan1213 <78837755+Wilsonchan1213@users.noreply.github.com>
2023-11-13 23:15:57 +0800

    add question database

---
+++ lib/image.dart
@@ -0,0 +1,19 @@
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:flutter/services.dart';
+
+Future<String> saveImageToStorageFromAssets(String assetPath, var n) async {
+  print("hi");
+  final byteData = await rootBundle.load(assetPath);
+  final directory = await getApplicationDocumentsDirectory();
+  final imagePath = '${directory.path}/image$n.jpg'; // Replace "my_image.jpg" with a unique name for your image
+  final newImageFile = File(imagePath);
+  print("hi");
+  try {
+    await newImageFile.writeAsBytes(byteData.buffer.asUint8List());
+    return imagePath;
+  } catch (e) {
+    print('Failed to save image to storage: $e');
+    return '';
+  }
+}
